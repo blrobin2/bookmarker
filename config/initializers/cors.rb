285da5cd5ef6beaceb7961dbac91028aaab69283
Bennie Robinson <brobinson88@gmail.com>
2020-08-18 08:14:48 -0400

    Tighten up CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,8 +9,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*',
+    origins 'http://localhost:3000'
+    resource '/api/v1/*',
              headers: :any,
              methods: %i[get post put patch delete options head]
   end
